105145eaf8c02cd85cc2362dffaf49403de1b30c
vumaasha <venkatesh.umaashankar@gmail.com>
2013-02-22 18:21:00 +0100

    improved cloze notes creation.

--- import.py
+++ import.py
@@ -3,8 +3,21 @@ import nltk
 import re
 from nltk.corpus import stopwords
 
+
+def stem_sentence(sentence):
+    parts = re.split('\s',sentence)
+    notes_check = re.compile(r'\[[^\]+]')
+    for part in parts:
+        if (re.search(notes_check, part) == None):
+            vocab_stem_list.append(germanStemmer.stem((re.sub(r'[\.,\?,!\'"]','',part)).lower()))
+
+def no_of_words(de_part):
+    parts = re.split('\s',de_part)
+    return len(parts)
+
 stop_words = set(stopwords.words("german"))
 stop_words.add('[ugs.]')
+stop_words.remove('mein')
 
 file_to_import='/home/venkatesh/Documents/DeutschKurz/dict.cc/DE-EN-38130.txt'
 anki_import_file='/home/venkatesh/Documents/DeutschKurz/dict.cc/DE-EN-38130.anki_cards'
@@ -22,7 +35,10 @@ vocabMap = {}
 
 for line in vocab_list:
     line = line.rstrip('\r\n')
-    de_part,en_part=line.split('\t')
+    try:
+        de_part,en_part=line.split('\t')
+    except ValueError:
+        print "Error in parsing line :{0}. ignoring...".format(line)    
     vocabMap.setdefault(de_part,[])
     vocabMap[de_part].append(en_part)
     
@@ -34,8 +50,7 @@ for de_part,en_part in vocabMap.items():
         gender = gender_info.group(1)
         word = re.sub(r'{([^}]+)}','',de_part)
         word = re.sub(r'^(die|der|das)\s','',word)
-        for part in re.split('\s',de_part):
-            vocab_stem_list.append(germanStemmer.stem(part)) 
+        stem_sentence(de_part)
         if ( gender == 'm'):
             word = 'der '+word
             tag='mannlich'
@@ -49,15 +64,14 @@ for de_part,en_part in vocabMap.items():
             word = 'das '+word
             tag='neutral'
         anki_import.write(word+'\t'+', '.join(en_part)+'\t'+tag+'\n')
-    elif (sentence_check != None):
+    elif (sentence_check != None and no_of_words(de_part)>1):
+        #stem_sentence(de_part);
         sentence_list.append(de_part)
     else:
-        parts = re.split('\s',de_part)
-        if len(parts) > 2:
-                sentence_list.append(de_part)
+        if no_of_words(de_part) > 2:
+            sentence_list.append(de_part)
         else:
-            for part in parts:
-                vocab_stem_list.append(germanStemmer.stem(part))
+            stem_sentence(de_part);
             anki_import.write(de_part+'\t'+', '.join(en_part)+'\t'+''+'\n')
 
 anki_cloze_import = codecs.open(anki_import_cloze_file,encoding='utf-8',mode='w')
@@ -70,14 +84,21 @@ start_sent = '<div><span style=" font-style: normal; font-weight: normal;">'
 end_sent = '</span></div>'
 
 for de_part in sentence_list:
+#    if re.search(r'meinen',de_part):
+#        print 'debug'
     en_part=vocabMap[de_part]
     tokens = re.split('\s',de_part)
-    for i,token in enumerate(tokens):
-        stem = germanStemmer.stem(re.sub(r'[\.,\?,!\'"]','',token))
+    is_stem_found = False
+    for i,token in enumerate(tokens):        
+        stem = germanStemmer.stem((re.sub(r'[\.,\?,!\'"]','',token)).lower())
         if (stem in vocab_stem_set):
+            is_stem_found = True
             tokens[i]='{{c1::'+token+'}}'
     de_sent = ' '.join(tokens)
-    anki_cloze_import.write(', '.join(en_part)+' : '+de_sent+'\tsent\n')
+    if  is_stem_found:
+        anki_cloze_import.write(', '.join(en_part)+' : '+de_sent+'\tsent\n')
+    else:
+        anki_import.write(de_part+'\t'+', '.join(en_part)+'\t'+''+'\n')
 
 anki_import.flush()
 anki_import.close()
